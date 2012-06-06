d15b298364a4a1fa86dc9e8736c84832f724f573
yang2507366 <yang3800650071@163.com>
2012-06-06 09:39:00 +0800

    Initial Commit

---
+++ FieldListener/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FieldListener
+//
+//  Created by gewara on 12-6-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FieldListener/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  FieldListener
+//
+//  Created by gewara on 12-6-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
